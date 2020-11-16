490074aeb0032185e7b99fef2e36836afe8aa757
DESKTOP-HDASJFM\junae <junaetcse@gmail.com>
2020-11-16 13:29:20 +0600

    implement jwt token

--- app/User.php
+++ app/User.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App;
-
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -37,4 +37,19 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'details' => 'array'
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
