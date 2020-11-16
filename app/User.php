b88a1b44d18847611d5828949d4c1ecfd666c1f8
DESKTOP-HDASJFM\junae <junaetcse@gmail.com>
2020-11-16 11:12:23 +0600

    modify user model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname', 'email', 'password', 'lastname', 'details', 'is_active',
     ];
 
     /**
@@ -35,5 +35,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'details' => 'array'
     ];
 }
